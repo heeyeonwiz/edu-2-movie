0299a81fd87991cf9a6e02293b39c219ab7db7bf
starcell npark <npark@starcell.co.kr>
2021-02-16 20:55:45 +0900

    copy script 추가

---
+++ edu-movie-notebook.sh
@@ -0,0 +1,4 @@
+## notebook-edu-movie.sh
+cp /home/deepcell/edu-2-movie/*.ipynb /home/deepcell/notebook/5.movie-review/ 
+rm -rf /home/deepcell/edu-2-movie/
+ls -al /home/deepcell/notebook/5.movie-review
